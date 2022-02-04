06bf9982585605e955fa8a07011af7c4786b9e27
Danta <rsudanta20@gmail.com>
2022-02-05 01:27:14 +0700

    resolve bad request

--- app/Http/Controllers/API/MesinInferensiController.php
+++ app/Http/Controllers/API/MesinInferensiController.php
@@ -22,91 +22,84 @@ class MesinInferensiController extends Controller
                 'message' => 'Rules data is empty',
             ], 'Rules data is empty');
         }
-        try {
-            $input = json_decode($request->getContent(), true);
-            $gejalaUser = array();
+        $input = json_decode($request->getContent(), true);
+        $gejalaUser = array();
 
-            for ($x = 0; $x < count($input); $x++) {
-                array_push($gejalaUser, $input[$x]['id_gejala']);
-            }
+        for ($x = 0; $x < count($input); $x++) {
+            array_push($gejalaUser, $input[$x]['id_gejala']);
+        }
 
-            $id_penyakit = BasisPengetahuan::distinct()->pluck('id_penyakit');
+        $id_penyakit = BasisPengetahuan::distinct()->pluck('id_penyakit');
 
-            for ($x = 0; $x < count($id_penyakit); $x++) {
-                $penyakit[] = BasisPengetahuan::where('id_penyakit', $id_penyakit[$x])->get();
-            }
+        for ($x = 0; $x < count($id_penyakit); $x++) {
+            $penyakit[] = BasisPengetahuan::where('id_penyakit', $id_penyakit[$x])->get();
+        }
 
-            for ($x = 0; $x < count($penyakit); $x++) {
-                for ($z = 0; $z < count($penyakit[$x]); $z++) {
-                    if (in_array($penyakit[$x][$z]['id_gejala'], $gejalaUser)) {
-                        for ($y = 0; $y < count($input); $y++) {
-                            if ($penyakit[$x][$z]['id_gejala'] == $input[$y]['id_gejala']) {
-                                $penyakit[$x][$z]['nilai_cf'] *= $input[$y]['nilai_cf'];
-                            }
+        for ($x = 0; $x < count($penyakit); $x++) {
+            for ($z = 0; $z < count($penyakit[$x]); $z++) {
+                if (in_array($penyakit[$x][$z]['id_gejala'], $gejalaUser)) {
+                    for ($y = 0; $y < count($input); $y++) {
+                        if ($penyakit[$x][$z]['id_gejala'] == $input[$y]['id_gejala']) {
+                            $penyakit[$x][$z]['nilai_cf'] *= $input[$y]['nilai_cf'];
                         }
-                    } else {
-                        $penyakit[$x][$z]['nilai_cf'] *= 0;
                     }
+                } else {
+                    $penyakit[$x][$z]['nilai_cf'] *= 0;
                 }
             }
-            $hasilCombineTemp = array();
-            $hasilCF = array();
+        }
+        $hasilCombineTemp = array();
+        $hasilCF = array();
 
-            for ($x = 0; $x < count($penyakit); $x++) {
-                for ($z = 0; $z < count($penyakit[$x]); $z++) {
+        for ($x = 0; $x < count($penyakit); $x++) {
+            for ($z = 0; $z < count($penyakit[$x]); $z++) {
 
-                    if ($z == 0 && isset($penyakit[$x][$z + 1]) == false) {
-                        $hasilCombine = $penyakit[$x][$z]['nilai_cf'];
-                        array_push($hasilCombineTemp, $hasilCombine);
-                    } else if ($z == 0 && isset($penyakit[$x][$z + 1]) == true) {
-                        $hasilCombine = $penyakit[$x][$z]['nilai_cf'] + ($penyakit[$x][$z + 1]['nilai_cf'] * (1 - $penyakit[$x][$z]['nilai_cf']));
-                        array_push($hasilCombineTemp, $hasilCombine);
-                    } else if ($z < count($penyakit[$x]) - 1) {
-                        $hasilCombine = $hasilCombineTemp[0] + ($penyakit[$z][$z + 1]['nilai_cf'] * (1 - $hasilCombineTemp[0]));
-                        $hasilCombineTemp = [];
-                        array_push($hasilCombineTemp, $hasilCombine);
-                    }
-                }
-                array_push($hasilCF, $hasilCombineTemp);
-                $hasilCombineTemp = [];
-            }
-
-            for ($x = 0; $x < count($id_penyakit); $x++) {
-                $hasilDiagnosa[] = Penyakit::where('id', $id_penyakit[$x])->get();
-                $hasilDiagnosa[$x][0]['hasilCF'] = $hasilCF[$x][0];
-            }
-
-            $hasilMax = array();
-
-            for ($x = 0; $x < count($hasilDiagnosa); $x++) {
-                if ($x == 0) {
-                    array_push($hasilMax, $hasilDiagnosa[$x][0]);
-                } else if ($hasilDiagnosa[$x - 1][0]['hasilCF'] < $hasilDiagnosa[$x][0]['hasilCF']) {
-                    $hasilMax = [];
-                    array_push($hasilMax, $hasilDiagnosa[$x][0]);
+                if ($z == 0 && isset($penyakit[$x][$z + 1]) == false) {
+                    $hasilCombine = $penyakit[$x][$z]['nilai_cf'];
+                    array_push($hasilCombineTemp, $hasilCombine);
+                } else if ($z == 0 && isset($penyakit[$x][$z + 1]) == true) {
+                    $hasilCombine = $penyakit[$x][$z]['nilai_cf'] + ($penyakit[$x][$z + 1]['nilai_cf'] * (1 - $penyakit[$x][$z]['nilai_cf']));
+                    array_push($hasilCombineTemp, $hasilCombine);
+                } else if (isset($penyakit[$x][$z + 1]) == true) {
+                    $hasilCombine = $hasilCombineTemp[0] + ($penyakit[$x][$z + 1]['nilai_cf'] * (1 - $hasilCombineTemp[0]));
+                    $hasilCombineTemp = [];
+                    array_push($hasilCombineTemp, $hasilCombine);
                 }
             }
-            if ($hasilMax[0]['hasilCF'] == 0) {
-                $hasilMax = ["Hasil nilai keyakinan sama dengan nol"];
-                Hasil::create([
-                    'hasil_cf' => null,
-                    'id_penyakit' => null,
-                    'id_user' => Auth::id(),
-                ]);
-                return ResponseFormatter::success($hasilMax[0], 'Consultation Success');
-            } else {
-                Hasil::create([
-                    'hasil_cf' => $hasilMax[0]['hasilCF'],
-                    'id_penyakit' => $hasilMax[0]['id'],
-                    'id_user' => Auth::id(),
-                ]);
-                return ResponseFormatter::success($hasilMax[0], 'Consultation Success');
+            array_push($hasilCF, $hasilCombineTemp);
+            $hasilCombineTemp = [];
+        }
+
+        for ($x = 0; $x < count($id_penyakit); $x++) {
+            $hasilDiagnosa[] = Penyakit::where('id', $id_penyakit[$x])->get();
+            $hasilDiagnosa[$x][0]['hasilCF'] = $hasilCF[$x][0];
+        }
+
+        $hasilMax = array();
+
+        for ($x = 0; $x < count($hasilDiagnosa); $x++) {
+            if ($x == 0) {
+                array_push($hasilMax, $hasilDiagnosa[$x][0]);
+            } else if ($hasilDiagnosa[$x - 1][0]['hasilCF'] <= $hasilDiagnosa[$x][0]['hasilCF']) {
+                $hasilMax = [];
+                array_push($hasilMax, $hasilDiagnosa[$x][0]);
             }
-        } catch (Exception $e) {
-            return ResponseFormatter::error([
-                'message' => 'Something went wrong',
-                'error' => $e
-            ], $e);
+        }
+        if ($hasilMax[0]['hasilCF'] == 0) {
+            $hasilMax = ["Hasil nilai keyakinan sama dengan nol"];
+            Hasil::create([
+                'hasil_cf' => null,
+                'id_penyakit' => null,
+                'id_user' => Auth::id(),
+            ]);
+            return ResponseFormatter::success($hasilMax[0], 'Consultation Success');
+        } else {
+            Hasil::create([
+                'hasil_cf' => $hasilMax[0]['hasilCF'],
+                'id_penyakit' => $hasilMax[0]['id'],
+                'id_user' => Auth::id(),
+            ]);
+            return ResponseFormatter::success($hasilMax[0], 'Consultation Success');
         }
     }
 
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/foo', function () {
-    Artisan::call('storage:link');
-});
-
 Route::get('/', function () {
     return redirect()->route('dashboard');
 });
