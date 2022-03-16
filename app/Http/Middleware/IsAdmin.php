3bb5292aa704a35b505dffdb02638d2670adc2ea
Danta <rsudanta20@gmail.com>
2022-03-17 04:43:55 +0700

    auth admin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Contracts\LogoutResponse;
+use Illuminate\Contracts\Auth\StatefulGuard;
 
 class IsAdmin
 {
@@ -15,11 +17,25 @@ class IsAdmin
      * @param  \Closure  $next
      * @return mixed
      */
+    protected $guard;
+    public function __construct(StatefulGuard $guard)
+    {
+        $this->guard = $guard;
+    }
+
+    public function destroy(Request $request): LogoutResponse
+    {
+        $this->guard->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return app(LogoutResponse::class);
+    }
+
     public function handle(Request $request, Closure $next)
     {
         if (Auth::user() && Auth::user()->role == 'ADMIN') {
             return $next($request);
         }
-        return redirect('/login');
+        return $this->destroy($request);
     }
 }
